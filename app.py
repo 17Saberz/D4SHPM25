2a20c971fa61b84e4f01e9426b2da06523d42a89
Peeranat Pathomkul <focus.peeranat@gmail.com>
2024-03-06 10:51:17 +0700

    Add 1

--- app.py
+++ app.py
@@ -111,7 +111,6 @@ def update_charts(start_date, end_date):
                 "hovertemplate": "PM25%{y:.2f}<extra></extra>",
             },
         ],
-    
         "layout": {
             "title": {
                 "text": "PM25",  # Adjusted the chart title
