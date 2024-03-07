7110f93cbd60f7a160809b17dafb4d2a21471ab1
Nong9 <koonnai009@gmail.com>
2024-03-07 10:33:46 +0700

    Data clean for training

--- air4thai.py
+++ air4thai.py
@@ -18,13 +18,35 @@ response_json = response.json()
 
 pd_from_dict = pd.DataFrame.from_dict(response_json["stations"][0]["data"])
 print(pformat(pd_from_dict))
-df = pd.DataFrame(pd_from_dict)
-column_null = ['PM25','CO','NO2','TEMP','RH']
-df1 = df[column_null] = df[column_null].fillna(df[column_null].mean().round(2))
-df1.insert(0, 'DATETIMEDATA', df['DATETIMEDATA'])
-condition = (df1[column_null] == 0).sum(axis=1) > 1
-df1.drop(df1[condition].index, inplace=True)
-df1["NO2"].replace(0, df1["NO2"].mean().round(2))
 
-print(pformat(df1))
-df1.to_csv('HKT_clean.csv', index=True)
+# df = pd.DataFrame(pd_from_dict)
+# column_null = ['PM25','CO','NO2','TEMP','RH']
+# df1 = df[column_null] = df[column_null].fillna(df[column_null].mean().round(2))
+# df1.insert(0, 'DATETIMEDATA', df['DATETIMEDATA'])
+# condition = (df1[column_null] == 0).sum(axis=1) > 1
+# df1.drop(df1[condition].index, inplace=True)
+# df1["NO2"].replace(0, df1["NO2"].mean().round(2))
+# df1.to_csv('HKT_clean.csv', index=True)
+
+# df1[['Date', 'Time']] = df1['DATETIMEDATA'].str.split(' ', expand=True)
+# df3 = df1.drop('DATETIMEDATA', axis=1)
+# df3 = df1.groupby(['Date']).mean().round(2)
+# df3.to_csv('mean_value_nai.csv', index=True)
+
+df_train = pd.read_csv("HKT_clean.csv")
+# Set data for predict next day
+for i in range(len(df_train) - 1):
+    df_train.loc[i, 'DAY1'] = df_train.loc[i+1, 'PM25']
+df_train.loc[len(df_train) - 1, 'DAY1'] = float('nan')
+
+# Set data for predict next 2 day
+for i in range(len(df_train) - 2):
+    df_train.loc[i, 'DAY2'] = df_train.loc[i+2, 'PM25']
+df_train.loc[len(df_train) - 2, 'DAY2'] = float('nan')
+
+# Set data for predict next 3 day
+for i in range(len(df_train) - 3):
+    df_train.loc[i, 'DAY3'] = df_train.loc[i+3, 'PM25']
+df_train.loc[len(df_train) - 3, 'DAY3'] = float('nan')
+
+print(df_train)
